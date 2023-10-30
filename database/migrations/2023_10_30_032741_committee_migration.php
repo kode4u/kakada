d1599cc8424627aa9b712560bf0bddc9dfed0f3b
ENG TITYA <eng.titya@nubb.edu.kh>
2023-10-30 15:34:02 +0700

    ok

--- database/migrations/2023_10_30_032741_committee_migration.php
+++ database/migrations/2023_10_30_032741_committee_migration.php
@@ -10,7 +10,7 @@ return new class extends Migration {
      */
     public function up(): void
     {
-        Schema::create('committees2', function (Blueprint $table) {
+        Schema::create('committees', function (Blueprint $table) {
             $table->id();
             $table->string('name')->nullable();
             $table->date('dob')->nullable();
