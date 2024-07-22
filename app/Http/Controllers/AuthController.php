d6eb94d32cc17f7818be0c9fcaa0570a88bf3ebe
kode4u <info.kode4u@gmail.com>
2024-07-23 00:44:04 +0700

    add api

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -13,7 +13,6 @@ class AuthController extends Controller
     public function login(Request $request)
     {
         $credentials = $request->only('email', 'password');
-        dd($credentials);
         if (Auth::attempt($credentials)) {
             dd('auth if');
 
