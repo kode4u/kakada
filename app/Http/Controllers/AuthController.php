ad2642f61b017cb4083e7f9f1c3d7137a6d316e6
kode4u <info.kode4u@gmail.com>
2024-07-23 00:42:53 +0700

    add api

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -6,14 +6,17 @@ use Illuminate\Support\Facades\Hash;
 use Illuminate\Support\Facades\Auth;
 use App\Models\User;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Log;
 
 class AuthController extends Controller
 {
     public function login(Request $request)
     {
         $credentials = $request->only('email', 'password');
-
+        dd($credentials);
         if (Auth::attempt($credentials)) {
+            dd('auth if');
+
             $user = Auth::user();
             $token = $user->createToken('API Token')->plainTextToken;
             Log::info('Token created:', ['token' => $token]);
