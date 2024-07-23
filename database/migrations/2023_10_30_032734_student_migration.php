2f85ec5a86416826393344d31c26ea862e4ffba0
titya-it <122963459+titya-it@users.noreply.github.com>
2024-07-23 17:27:17 +0700

    ok

--- database/migrations/2023_10_30_032734_student_migration.php
+++ database/migrations/2023_10_30_032734_student_migration.php
@@ -12,16 +12,39 @@ return new class extends Migration {
     {
         Schema::create('students', function (Blueprint $table) {
             $table->id();
+            $table->string('candid')->nullable();
             $table->string('name')->nullable();
             $table->string('gender')->nullable();
             $table->date('dob')->nullable();
-            $table->string('class')->nullable();
-            $table->string('school')->nullable();
-            $table->string('exam_center')->nullable();
-            $table->string('language')->nullable();
-            $table->string('room_no')->nullable();
-            $table->string('table_no')->nullable();
-            $table->string('province')->nullable();
+            $table->string('fathername')->nullable();
+            $table->string('mothername')->nullable();
+            $table->string('photo')->nullable();
+            $table->string('schoolid')->nullable();
+            $table->string('room')->nullable();
+            $table->string('seat')->nullable();
+            $table->string('kessgrade')->nullable();
+            $table->string('mathgrade')->nullable();
+            $table->string('biolgrade')->nullable();
+            $table->string('geoggrade')->nullable();
+            $table->string('histgrade')->nullable();
+            $table->string('morcivgrade')->nullable();
+            $table->string('chemgrade')->nullable();
+            $table->string('physgrade')->nullable();
+            $table->string('earthgrade')->nullable();
+            $table->string('lang_grade')->nullable();
+            $table->double('percentile')->nullable();
+            $table->integer('letternumber')->nullable();
+            $table->string('pass')->nullable();
+            $table->string('grade')->nullable();
+            $table->integer('program')->nullable();
+            $table->integer('candtypecode')->nullable();
+            $table->string('examcenter')->nullable();
+            $table->string('provincek')->nullable();
+            $table->string('programkh')->nullable();
+            $table->string('genderl')->nullable();
+            $table->string('pprefixk')->nullable();
+            $table->string('provinceks')->nullable();
+            $table->string('schoolnamekh')->nullable();
         });
     }
 
