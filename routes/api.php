d76b805409306f81fb7138b769c4a8594e616ebb
titya-it <122963459+titya-it@users.noreply.github.com>
2024-07-24 00:22:13 +0700

    ok

--- routes/api.php
+++ routes/api.php
@@ -23,5 +23,11 @@ Route::middleware('auth:sanctum')->group(function () {
     Route::post('/student', [StudentController::class, 'index']);
     Route::post('/check', [CheckController::class, 'index']);
     Route::post('/category', [CheckCategoryController::class, 'index']);
+
+    Route::get('/check', [CheckController::class, 'index'])->name('checks.index');
+    Route::post('/check', [CheckController::class, 'store'])->name('checks.store');
+    Route::get('/check/{id}', [CheckController::class, 'show'])->name('checks.show');
+    Route::put('/check/{id}', [CheckController::class, 'update'])->name('checks.update');
+    Route::delete('/check/{id}', [CheckController::class, 'destroy'])->name('checks.destroy');
     // Add other student-related routes here
 });
