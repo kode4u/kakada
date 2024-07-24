a4167da23c949f80acdffe995f23e31f3575bd96
kode4u <info.kode4u@gmail.com>
2024-07-24 23:10:41 +0700

    OK

--- database/migrations/2024_07_24_004226_create_checks_table.php
+++ database/migrations/2024_07_24_004226_create_checks_table.php
@@ -16,6 +16,7 @@ return new class extends Migration
             $table->integer('student_id');
             $table->integer('category_id');
             $table->integer('user_id');
+            $table->integer('correct');
             $table->timestamps();
             $table->foreign('user_id')->references('id')->on('users')->onDelete('cascade');
             $table->foreign('category_id')->references('id')->on('categories')->onDelete('cascade');
